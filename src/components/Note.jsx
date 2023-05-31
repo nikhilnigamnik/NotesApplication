0f4b45d86705125fb4864f3c5180adfccc5aa6f4
nikhilnigamnik <nikhilnigamnik@gmail.com>
2023-05-31 22:06:00 +0530

    ui updated

--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -52,7 +52,7 @@ function Note(props) {
   return (
     <div className="note" style={{ backgroundColor: props.note.color }}>
       <textarea
-        className="note_text"
+        className="note_text text-gray-800"
         defaultValue={props.note.text}
         onChange={(event) => updateText(event.target.value, props.note.id)}
       />
